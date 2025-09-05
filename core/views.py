8691050a2ce57fa9f9f4d3c64a5f2cbd88e86f20
stonefullstm <stonefull.stm@gmail.com>
2025-09-05 09:50:27 -0300

    fix: Altera view para buscar os dados de históricos jah considerando que a atualizacao eh feita em background

--- core/views.py
+++ core/views.py
@@ -56,50 +56,50 @@ class HistoryViewSet(viewsets.ViewSet):
 
     @action(detail=False, methods=["get"])
     def get_last_day(self, request):
-        tickers = StockSerializer(Stock.objects.all(), many=True).data
-        ticker_list = [ticker["ticker"] + ".SA" for ticker in tickers]
-        config = WalletConfig.objects.first()
-        if config:
-            if config.history_date != date.today():
-                multiple_tickers = yf.download(
-                    ticker_list,
-                    period="2d",
-                    interval="1d",
-                    multi_level_index=False,
-                    ignore_tz=True,
-                    auto_adjust=False,
-                )[["Close", "Volume"]].T.dropna()
-                for index, row in multiple_tickers.iterrows():
-                    if index[0] == "Close":
-                        HistoryStock.objects.update_or_create(
-                            ticker=index[1].replace(".SA", ""),
-                            defaults={
-                                "previous_close": float(row.iloc[0]),
-                                "actual_close": float(row.iloc[1]),
-                                "alta_baixa": float(
-                                    (
-                                        (row.iloc[1] - row.iloc[0])
-                                        / row.iloc[1]
-                                    ) * 100
-                                ),
-                                "volume": float(
-                                    multiple_tickers.loc[
-                                        ("Volume", index[1])].iloc[1]
-                                ),
-                                "date": multiple_tickers.columns[1].strftime(
-                                    "%Y-%m-%d"
-                                ),
-                            },
-                        )
-                config.history_date = date.today()
-                config.save()
-            return Response(
+        # tickers = StockSerializer(Stock.objects.all(), many=True).data
+        # ticker_list = [ticker["ticker"] + ".SA" for ticker in tickers]
+        # config = WalletConfig.objects.first()
+        # if config:
+        #     if config.history_date != date.today():
+        #         multiple_tickers = yf.download(
+        #             ticker_list,
+        #             period="2d",
+        #             interval="1d",
+        #             multi_level_index=False,
+        #             ignore_tz=True,
+        #             auto_adjust=False,
+        #         )[["Close", "Volume"]].T.dropna()
+        #         for index, row in multiple_tickers.iterrows():
+        #             if index[0] == "Close":
+        #                 HistoryStock.objects.update_or_create(
+        #                     ticker=index[1].replace(".SA", ""),
+        #                     defaults={
+        #                         "previous_close": float(row.iloc[0]),
+        #                         "actual_close": float(row.iloc[1]),
+        #                         "alta_baixa": float(
+        #                             (
+        #                                 (row.iloc[1] - row.iloc[0])
+        #                                 / row.iloc[1]
+        #                             ) * 100
+        #                         ),
+        #                         "volume": float(
+        #                             multiple_tickers.loc[
+        #                                 ("Volume", index[1])].iloc[1]
+        #                         ),
+        #                         "date": multiple_tickers.columns[1].strftime(
+        #                             "%Y-%m-%d"
+        #                         ),
+        #                     },
+        #                 )
+        #         config.history_date = date.today()
+        #         config.save()
+        return Response(
                 MaxMinSerializer(
                     HistoryStock.objects.all(), many=True
                 ).data
             )
-        return Response(
-            {"error": "No wallet configuration found."}, status=404)
+        # return Response(
+        #     {"error": "No wallet configuration found."}, status=404)
 
 
 class StockViewSet(APIView):
